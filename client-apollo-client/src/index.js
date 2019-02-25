a3898e848686a84e4e9ef11c1606a972b6e0e2ea
Dan <bluedusk@qq.com>
2019-02-25 17:09:48 +1100

    init

--- client-apollo-client/src/index.js
+++ client-apollo-client/src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
 import { createUploadLink } from 'apollo-upload-client';
-// import { ApolloClient } from 'apollo-client';
+import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const client = new ApolloClient({
-  uri: "http://localhost:4001"
-});
-// const link = createUploadLink({ uri: 'http://localhost:4001' });
+const link = createUploadLink({ uri: 'http://localhost:4001' });
 
-// const client = new ApolloClient({
-//   uri: 'http://localhost:4001',
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
